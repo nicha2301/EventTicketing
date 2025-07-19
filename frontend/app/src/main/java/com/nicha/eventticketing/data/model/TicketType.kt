ed932ea61acb0abba2cdf4dd22178be119a20911
Nguyen Quoc Trung <nqt230103a@gmail.com>
2025-07-19 23:00:40 +0700

    feat: delete unused file

--- frontend/app/src/main/java/com/nicha/eventticketing/data/model/TicketType.kt
+++
@@ -1,9 +0,0 @@
-package com.nicha.eventticketing.data.model
-
-data class TicketType(
-    val id: String,
-    val name: String,
-    val price: Double,
-    val description: String,
-    val availableQuantity: Int
-) 
